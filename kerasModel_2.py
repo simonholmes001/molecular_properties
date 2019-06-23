ced5ee4268dee76a34653a882a4160093a6bcbd4
simonholmes001 <simonholmesabc@gmail.com>
2019-06-23 15:58:33 +0000

    updates on code 2

--- kerasModel_2.py
+++ kerasModel_2.py
@@ -1,6 +1,11 @@
+from datetime import datetime
+startTime = datetime.now()
+
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+
+import pandas as pd
 
 from keras.models import Sequential
 from keras.layers.normalization import BatchNormalization
@@ -15,14 +20,14 @@ from keras.models import Model
 
 from sklearn.model_selection import train_test_split
 
-features = pd.DataFrame("small_features.csv")
+features = pd.read_csv("small_features.csv")
 X = features.drop(["fc", "sd", "pso", "dso", "scalar_coupling_constant"], axis=1)
 y = pd.DataFrame(data = features, columns=["id","scalar_coupling_constant"])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
 model = tf.keras.Sequential()
 
-model.add(layers.Dense(64, input_dim = 55, activation='relu', kernel_regularizer=tf.keras.regularizers.l1(0.01),
+model.add(layers.Dense(64, input_dim = 50, activation='relu', kernel_regularizer=tf.keras.regularizers.l1(0.01),
             kernel_initializer='orthogonal', bias_initializer=tf.keras.initializers.constant(2.0)))
 model.add(layers.Dense(32, activation='relu', kernel_regularizer=tf.keras.regularizers.l1(0.01),
             kernel_initializer='orthogonal', bias_initializer=tf.keras.initializers.constant(2.0)))
@@ -42,3 +47,5 @@ model.fit(X_train, y_train,
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
+print('\nTime elasped: ', datetime.now() - startTime)
